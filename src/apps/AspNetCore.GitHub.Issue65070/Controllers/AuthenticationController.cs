09b3ad24a0772b40c647c25142a1aca64524dc4d
Lasse Herget <Lasse.Herget@swisssalary.ch>
2026-01-16 10:50:30 +0100

    Add sign-in page with login hint and redirect support
    Introduces a new Blazor sign-in page at /SignIn with a form that collects a login hint (email) and redirect URI. The form posts to the authentication challenge endpoint and includes antiforgery protection. Parameters are populated from query strings, and routing is configured for the new page.

--- src/apps/AspNetCore.GitHub.Issue65070/Controllers/AuthenticationController.cs
+++ src/apps/AspNetCore.GitHub.Issue65070/Controllers/AuthenticationController.cs
@@ -11,6 +11,7 @@ public class AuthenticationController : Controller
     /// <param name="loginHint">The login hint. This would typically be the email address to authenticate.</param>
     /// <param name="redirectUri">The URL to redirect to after authentication.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
+    [ValidateAntiForgeryToken]
     [Route(EndpointPaths.SignInChallenge)]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public async Task<IActionResult> ChallengeAsync(string? loginHint = null, string? redirectUri = null) => this.Challenge(
---
+++ src/apps/AspNetCore.GitHub.Issue65070/Pages/SignIn/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AspNetCore.GitHub.Issue65070.Pages.SignIn;
+
+/// <summary>Represents the page that triggers authentication challenges.</summary>
+[Route(EndpointPaths.SignIn)]
+public partial class Index : ComponentBase
+{
+    /// <summary>Gets or sets the email address.</summary>
+    /// <value>The email address of the user.</value>
+    [Parameter]
+    [SupplyParameterFromQuery]
+    public string? LoginHint { get; set; }
+
+    /// <summary>Gets or sets the URL to return to, once the user is authenticated.</summary>
+    /// <value>The path of the endpoint to return to, once the user is authenticated.</value>
+    [Parameter]
+    [SupplyParameterFromQuery]
+    public string? RedirectUri { get; set; }
+}
