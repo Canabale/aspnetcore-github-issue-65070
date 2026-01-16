cf2b2d6a18626d6eeb428a3b0a6acc14cd8e5fe7
Lasse Herget <Lasse.Herget@swisssalary.ch>
2026-01-16 10:46:04 +0100

    Add sign-out Razor page with route and message
    Introduced a new Razor page for signing out users under the AspNetCore.GitHub.Issue65070.Pages.SignOut namespace. The page displays a "We're signing you out..." message and is accessible via the EndpointPaths.SignOut route. Added the corresponding partial class with appropriate routing and documentation.

---
+++ src/apps/AspNetCore.GitHub.Issue65070/Pages/SignOut/Index.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AspNetCore.GitHub.Issue65070.Pages.SignOut;
+
+/// <summary>Represents the page that is shown when the user signs out.</summary>
+[Route(EndpointPaths.SignOut)]
+public partial class Index : ComponentBase
+{
+}
