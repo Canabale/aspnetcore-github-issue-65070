0e0c9317b948b5247ec9aa9d885ba8e9d68f2aa3
Lasse Herget <Lasse.Herget@swisssalary.ch>
2026-01-16 10:32:19 +0100

    Add startup

---
+++ src/apps/AspNetCore.GitHub.Issue65070/EndpointPaths.cs
@@ -0,0 +1,17 @@
+﻿namespace AspNetCore.GitHub.Issue65070;
+
+/// <summary>Provides the paths of the endpoints of this app.</summary>
+public static class EndpointPaths
+{
+    /// <summary>The path that the application redirects to, when an authentication process succeeded.</summary>
+    public const string Authenticated = "/authenticated";
+
+    /// <summary>The path of the sign-in endpoints.</summary>
+    public const string SignIn = "/signin";
+
+    /// <summary>The path of the sign-in challenge endpoint.</summary>
+    public const string SignInChallenge = SignIn + "/challenge";
+
+    /// <summary>The endpoint the signs out the user.</summary>
+    public const string SignOut = "/signout";
+}
---
+++ src/apps/AspNetCore.GitHub.Issue65070/Program.cs
@@ -0,0 +1,76 @@
+﻿using AspNetCore.GitHub.Issue65070;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.CookiePolicy;
+
+// Create the web application builder.
+var webApplicationBuilder = WebApplication.CreateBuilder(args);
+webApplicationBuilder.Configuration.AddEnvironmentVariables();
+webApplicationBuilder.Configuration.AddJsonFile("appsettings.user.json", optional: true);
+
+// Configure Services
+webApplicationBuilder.Services.AddHttpContextAccessor();
+webApplicationBuilder.Services.AddLogging(options => options.AddConsole());
+webApplicationBuilder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.HttpOnly = HttpOnlyPolicy.Always;
+    options.Secure = CookieSecurePolicy.Always;
+    options.MinimumSameSitePolicy = SameSiteMode.Lax; // Required for correlation in OAuth.
+    options.ConsentCookie.IsEssential = false; // By default, cookies will require consent from the user.
+});
+
+var mvcBuilder = webApplicationBuilder.Services.AddRazorPages();
+var blazorBuilder = webApplicationBuilder.Services.AddServerSideBlazor(options =>
+{
+    options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(5);
+    options.DisconnectedCircuitMaxRetained = 100;
+});
+
+var authenticationBuilder = webApplicationBuilder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.LoginPath = EndpointPaths.SignIn;
+        options.AccessDeniedPath = EndpointPaths.SignIn;
+        options.LogoutPath = EndpointPaths.SignOut;
+
+        options.Cookie.SameSite = SameSiteMode.Strict;
+        options.Cookie.IsEssential = true;
+        webApplicationBuilder.Configuration.GetSection("Authentication:Scheme").Bind(options);
+    })
+    .AddMicrosoftAccount(options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.CallbackPath = EndpointPaths.Authenticated;
+        webApplicationBuilder.Configuration.GetSection("Authentication:IdentityProvider").Bind(options);
+    });
+
+webApplicationBuilder.Services.AddCascadingAuthenticationState();
+webApplicationBuilder.Services.AddAuthorization();
+webApplicationBuilder.Services.AddAntiforgery(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.Strict;
+    options.Cookie.IsEssential = true;
+});
+
+// Configure Request Pipeline
+var webApplication = webApplicationBuilder.Build();
+webApplication.UseHsts();
+webApplication.UseHttpsRedirection();
+webApplication.UseStaticFiles();
+webApplication.UseRouting();
+webApplication.UseCookiePolicy();
+webApplication.UseSession();
+webApplication.UseAuthentication();
+webApplication.UseAuthorization();
+webApplication.UseAntiforgery();
+
+// Map Endpoints
+webApplication.MapControllers();
+webApplication.MapBlazorHub(options => options.CloseOnAuthenticationExpiration = true);
+webApplication.MapFallbackToPage("/_Host");
+
+// Run the application
+await webApplication.RunAsync();
